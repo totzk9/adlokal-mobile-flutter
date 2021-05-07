ade88535a3f0e02d61486484fc9568608ba253a4
Tyrone Chris Abad <tyronechris67@gmail.com>
2021-05-07 17:37:11 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/tyrn/adlokal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tyrn.adlokal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+//import 'package:firebase_analytics/firebase_analytics.dart';
+//import 'package:firebase_analytics/observer.dart';
+import './controllers/controllers.dart';
+import './constants/constants.dart';
+import './ui/components/components.dart';
+import './helpers/helpers.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await GetStorage.init();
+  Get.put<AuthController>(AuthController());
+  Get.put<ThemeController>(ThemeController());
+  Get.put<LanguageController>(LanguageController());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ThemeController.to.getThemeModeFromStore();
+    return GetBuilder<LanguageController>(
+      builder: (languageController) => Loading(
+        child: GetMaterialApp(
+          translations: Localization(),
+          locale: languageController.getLocale,
+          // <- Current locale
+          navigatorObservers: [
+            // FirebaseAnalyticsObserver(analytics: FirebaseAnalytics()),
+          ],
+          debugShowCheckedModeBanner: false,
+          //defaultTransition: Transition.fade,
+          theme: AppThemes.lightTheme,
+          darkTheme: AppThemes.darkTheme,
+          themeMode: ThemeMode.system,
+          initialRoute: "/",
+          getPages: AppRoutes.routes,
+        ),
+      ),
+    );
+  }
+}
